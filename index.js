4edfcf4d5447e354a9ed28e15c410726c4b9aa26
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-01-08 20:57:04 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const porta = process.env.PORT || 3001;
-
-
-app.listen(porta, () =>{
-    console.log(`You dit it, server running on port ${porta}`)
-})
